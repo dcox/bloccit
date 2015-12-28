166b8423730e1e8b0aaa4e5cc8f65a525240b32e
Desiree Cox <dcox@zendesk.com>
2015-12-28 15:50:14 -0800

    Assignment 31 Attempted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  def make_spam
+    Posts.all.each { |post|
+      if post.id = 1
+        post.title = "SPAM"
+        post.save!
+      end
+    }
+  end
+
 end
