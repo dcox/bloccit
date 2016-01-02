2c882ab9e4bc571d0535c5172cf99969e3913069
Desiree Cox <dcox@zendesk.com>
2016-01-02 15:58:02 -0800

    assignment 33 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,17 @@ posts = Post.all
   )
 end
 
+# Create questions
+50.times do
+  Question.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: rand(1)
+  )
+end
+questions = Question.all
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
