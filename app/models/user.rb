b8e6110ed3d1b9980ff99b8dbc209f1ecb2fab1a
Desiree <dcox@zendesk.com>
2016-02-22 17:56:49 -0800

    Checkpoint 44 In Progress (Left off on 'Sending Email Notifications')

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -17,4 +18,9 @@ class User < ActiveRecord::Base
   has_secure_password
 
   enum role: [:member, :admin]
+
+  def favorite_for(post)
+    favorites.where(post_id: post.id).first
+  end
+
 end
