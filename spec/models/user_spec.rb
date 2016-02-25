6693cbdf8092d0dd77218c434ef5a67551891477
Desiree <dcox@zendesk.com>
2016-02-24 17:33:57 -0800

    Checkpoint 48 Complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,4 +64,11 @@ RSpec.describe User, type: :model do
       expect(user.favorite_for(@post)).to eq(favorite)
     end
   end
+
+  describe "#generate_auth_token" do
+    it "creates a token" do
+      expect(user.auth_token).to_not be_nil
+    end
+  end
+
 end
